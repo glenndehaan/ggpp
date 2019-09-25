eaff9afde8ef8ed10c7697db2e64d22ab8310f7e
Glenn de Haan <glenn@dehaan.cloud>
2019-09-25 09:05:25 +0200

    Added basic program args handling

--- src/ggpp.js
+++ src/ggpp.js
@@ -1,1 +1,27 @@
-console.log('HI!');
+const program = require('commander');
+
+/**
+ * Set program version
+ */
+program.version('0.0.1');
+
+/**
+ * Set program options
+ */
+program
+    .option('-s, --server', 'starts the registry server')
+    .option('-d, --debug', 'output debugging information');
+
+/**
+ * Let commander handle process arguments
+ */
+program.parse(process.argv);
+
+/**
+ * Check if we want to run the server or client
+ */
+if(program.server) {
+    console.log('Run server!');
+} else {
+    console.log('Run client!');
+}
