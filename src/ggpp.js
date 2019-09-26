166646eb593537e498fa541bdf0b97f26f48071b
Glenn de Haan <glenn@dehaan.cloud>
2019-09-26 08:44:42 +0200

    Fix snapcraft release? Again?

--- src/ggpp.js
+++ src/ggpp.js
@@ -43,7 +43,7 @@ log.debug('Warning!!! | Application runs in debug mode! | Warning!!!');
 /**
  * Set program version
  */
-program.version('0.1.1');
+program.version('0.1.2');
 
 /**
  * Set program options
